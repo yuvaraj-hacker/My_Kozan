ac2cd3d4865c82618c488929ce63cc7cbb02d0e0
krishnaraj-cyber <krishnarajtcz@gmail.com>
2024-09-23 13:01:22 +0530

    productpage / krishnaraj

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -6,6 +6,7 @@ import Wishlist from "../Components/Wishlist/Wishlist";
 import Cart from "../Components/Cart/Cart";
 import SignUp from "../Components/SignUp/SignUp";
 import ProductCard from "../Components/Products/ProductCard";
+import ProductPage from "../Components/ProductPage/ProductPage";
 export default function Approuter() {
     return (
         <>
@@ -15,6 +16,7 @@ export default function Approuter() {
                         <Route path="/" element={<Home />} />
                         <Route path="/aboutus" element={<About />} />
                         <Route path="/products" element={<ProductCard/>} />
+                        <Route path="/productpage" element={<ProductPage/>} />
                         <Route path="/wishlist" element={<Wishlist />} />
                         <Route path="/cart" element={<Cart />} />
                         <Route path="/signup" element={<SignUp />} />
